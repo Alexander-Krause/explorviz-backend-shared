bd0ae83cfc92d0a4173b404e33f22d082ef3a1a7
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2019-02-13 17:25:24 +0100

    added events to the landscape data model

---
+++ landscape-model/src/main/java/net/explorviz/shared/landscape/model/event/Event.java
@@ -0,0 +1,64 @@
+package net.explorviz.shared.landscape.model.event;
+
+import com.github.jasminb.jsonapi.annotations.Relationship;
+import com.github.jasminb.jsonapi.annotations.Type;
+
+import net.explorviz.shared.landscape.model.helper.BaseEntity;
+import net.explorviz.shared.landscape.model.landscape.Landscape;
+
+/**
+ * Model representing an event occurring in a {@link Landscape}.
+ */
+@SuppressWarnings("serial")
+@Type("event")
+public class Event extends BaseEntity {
+
+	@Relationship("parent")
+	private Landscape parent;
+
+	private long timestamp;
+
+	EEventType eventType = EEventType.UNKNOWN;
+
+	String eventMessage;
+
+	public Event(Landscape landscape, long timestamp, EEventType eventType, String eventMessage) {
+		this.parent = landscape;
+		this.timestamp = timestamp;
+		this.eventType = eventType;
+		this.eventMessage = eventMessage;
+	}
+
+	public Landscape getParent() {
+		return parent;
+	}
+
+	public void setParent(Landscape parent) {
+		this.parent = parent;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public EEventType getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(EEventType eventType) {
+		this.eventType = eventType;
+	}
+
+	public String getEventMessage() {
+		return eventMessage;
+	}
+
+	public void setEventMessage(String eventMessage) {
+		this.eventMessage = eventMessage;
+	}
+
+}
--- landscape-model/src/main/java/net/explorviz/shared/landscape/model/landscape/Landscape.java
+++ landscape-model/src/main/java/net/explorviz/shared/landscape/model/landscape/Landscape.java
@@ -1,13 +1,14 @@
 package net.explorviz.shared.landscape.model.landscape;
 
-import com.github.jasminb.jsonapi.annotations.Relationship;
-import com.github.jasminb.jsonapi.annotations.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+
+import com.github.jasminb.jsonapi.annotations.Relationship;
+import com.github.jasminb.jsonapi.annotations.Type;
+
 import net.explorviz.shared.landscape.model.application.Application;
 import net.explorviz.shared.landscape.model.application.ApplicationCommunication;
+import net.explorviz.shared.landscape.model.event.Event;
 import net.explorviz.shared.landscape.model.helper.BaseEntity;
 import net.explorviz.shared.landscape.model.store.Timestamp;
 
@@ -18,81 +19,74 @@ import net.explorviz.shared.landscape.model.store.Timestamp;
 @Type("landscape")
 public class Landscape extends BaseEntity {
 
-  @Relationship("timestamp")
-  private Timestamp timestamp;
+	@Relationship("timestamp")
+	private Timestamp timestamp;
 
-  @Relationship("systems")
-  private final List<System> systems = new ArrayList<>();
+	@Relationship("systems")
+	private final List<System> systems = new ArrayList<>();
 
-  private final Map<Long, String> events = new TreeMap<>();
-  private final Map<Long, String> exceptions = new TreeMap<>();
+	@Relationship("events")
+	private final List<Event> events = new ArrayList<>();
 
-  @Relationship("totalApplicationCommunications")
-  private List<ApplicationCommunication> totalApplicationCommunications = new ArrayList<>();
+	@Relationship("totalApplicationCommunications")
+	private List<ApplicationCommunication> totalApplicationCommunications = new ArrayList<>();
 
+	public Landscape() {
+		super();
+		this.timestamp = new Timestamp();
+	}
 
-  public Landscape() {
-    super();
-    this.timestamp = new Timestamp();
-  }
+	public Timestamp getTimestamp() {
+		return this.timestamp;
+	}
 
-  public Timestamp getTimestamp() {
-    return this.timestamp;
-  }
+	public void setTimestamp(final Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
 
-  public void setTimestamp(final Timestamp timestamp) {
-    this.timestamp = timestamp;
-  }
+	public List<System> getSystems() {
+		return this.systems;
+	}
 
-  public List<System> getSystems() {
-    return this.systems;
-  }
+	public List<Event> getEvents() {
+		return this.events;
+	}
 
-  public Map<Long, String> getEvents() {
-    return this.events;
-  }
+	public List<ApplicationCommunication> getTotalApplicationCommunications() {
+		return this.totalApplicationCommunications;
+	}
 
-  public Map<Long, String> getExceptions() {
-    return this.exceptions;
-  }
+	public void setTotalApplicationCommunications(final List<ApplicationCommunication> totalApplicationCommunications) {
+		this.totalApplicationCommunications = totalApplicationCommunications;
+	}
 
-  public List<ApplicationCommunication> getTotalApplicationCommunications() {
-    return this.totalApplicationCommunications;
-  }
+	/**
+	 * Clears all existing communication within the landscape.
+	 */
+	private void clearCommunication() {
 
-  public void setTotalApplicationCommunications(
-      final List<ApplicationCommunication> totalApplicationCommunications) {
-    this.totalApplicationCommunications = totalApplicationCommunications;
-  }
+		// keeps applicationCommunication, but sets it to zero requests
+		for (final ApplicationCommunication commu : this.getTotalApplicationCommunications()) {
+			commu.reset();
+		}
 
-  /**
-   * Clears all existing communication within the landscape.
-   */
-  private void clearCommunication() {
+		for (final System system : this.getSystems()) {
+			for (final NodeGroup nodegroup : system.getNodeGroups()) {
+				for (final Node node : nodegroup.getNodes()) {
+					for (final Application application : node.getApplications()) {
+						application.clearCommunication();
+					}
+				}
+			}
+		}
+	}
 
-    // keeps applicationCommunication, but sets it to zero requests
-    for (final ApplicationCommunication commu : this.getTotalApplicationCommunications()) {
-      commu.reset();
-    }
-
-    for (final System system : this.getSystems()) {
-      for (final NodeGroup nodegroup : system.getNodeGroups()) {
-        for (final Node node : nodegroup.getNodes()) {
-          for (final Application application : node.getApplications()) {
-            application.clearCommunication();
-          }
-        }
-      }
-    }
-  }
-
-  /**
-   * Resets the landscape.
-   */
-  public void reset() {
-    this.getExceptions().clear();
-    this.getEvents().clear();
-    this.clearCommunication();
-  }
+	/**
+	 * Resets the landscape.
+	 */
+	public void reset() {
+		this.getEvents().clear();
+		this.clearCommunication();
+	}
 
 }
