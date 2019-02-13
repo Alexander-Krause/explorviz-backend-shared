01a1fa8d8167d0095176c73a5c1c9bfe644b643f
Alexander Krause <akr@informatik.uni-kiel.de>
2019-02-13 08:23:44 +0100

    added landscape model.
    Co-authored-by: czirkelbach <czi@informatik.uni-kiel.de>

---
+++ landscape-model/src/main/java/net/explorviz/shared/landscape/model/helper/EProgrammingLanguage.java
@@ -0,0 +1,8 @@
+package net.explorviz.shared.landscape.model.helper;
+
+/**
+ * Enumeration representing supported programming languages within the data model.
+ */
+public enum EProgrammingLanguage {
+  JAVA, C, CPP, CSHARP, PERL, JAVASCRIPT, PYTHON, RUBY, PHP, UNKNOWN
+}
