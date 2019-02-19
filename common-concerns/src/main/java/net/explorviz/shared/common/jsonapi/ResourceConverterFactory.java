004af3df44fb010a1a5a0a2b5591d66087b87c34
Alexander Krause <akr@informatik.uni-kiel.de>
2019-02-19 11:59:10 +0100

    disable mandatory resource id for JSON API deserialization

--- common-concerns/src/main/java/net/explorviz/shared/common/jsonapi/ResourceConverterFactory.java
+++ common-concerns/src/main/java/net/explorviz/shared/common/jsonapi/ResourceConverterFactory.java
@@ -1,5 +1,6 @@
 package net.explorviz.shared.common.jsonapi;
 
+import com.github.jasminb.jsonapi.DeserializationFeature;
 import com.github.jasminb.jsonapi.ResourceConverter;
 import com.github.jasminb.jsonapi.SerializationFeature;
 import net.explorviz.shared.common.provider.GenericTypeFinder;
@@ -20,6 +21,9 @@ public class ResourceConverterFactory implements Factory<ResourceConverter> {
 
     this.converter = resourceConverter;
     this.converter.enableSerializationOption(SerializationFeature.INCLUDE_RELATIONSHIP_ATTRIBUTES);
+
+    // Clients send newly created records with no id, therefore allow this feature
+    this.converter.disableDeserializationOption(DeserializationFeature.REQUIRE_RESOURCE_ID);
   }
 
   @Override
