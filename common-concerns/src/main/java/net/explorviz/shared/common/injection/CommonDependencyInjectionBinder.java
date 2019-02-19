6c52cefe5fc29ecb843faf2c617579ec6d5baeb2
Alexander Krause <akr@informatik.uni-kiel.de>
2019-02-19 08:22:19 +0100

    rename DI binder and add ConfigInjectionResolver

--- common-concerns/src/main/java/net/explorviz/shared/common/injection/CommonDependencyInjectionBinder.java
+++ common-concerns/src/main/java/net/explorviz/shared/common/injection/CommonDependencyInjectionBinder.java
@@ -4,7 +4,9 @@ import com.github.jasminb.jsonapi.ResourceConverter;
 import javax.inject.Singleton;
 import net.explorviz.shared.common.jsonapi.ResourceConverterFactory;
 import net.explorviz.shared.config.annotations.Config;
+import net.explorviz.shared.config.annotations.ConfigValues;
 import net.explorviz.shared.config.annotations.injection.ConfigInjectionResolver;
+import net.explorviz.shared.config.annotations.injection.ConfigValuesInjectionResolver;
 import net.explorviz.shared.exceptions.ErrorObjectHelper;
 import net.explorviz.shared.security.TokenParserService;
 import org.glassfish.hk2.api.InjectionResolver;
@@ -14,10 +16,16 @@ import org.glassfish.hk2.utilities.binding.AbstractBinder;
 /**
  * Configures the dependency binding setup for inject during runtime.
  */
-public class DependencyInjectionBinder extends AbstractBinder {
+public class CommonDependencyInjectionBinder extends AbstractBinder {
 
   @Override
   public void configure() {
+
+    // Injectable config properties
+    this.bind(new ConfigInjectionResolver())
+        .to(new TypeLiteral<InjectionResolver<ConfigValues>>() {});
+    this.bind(new ConfigValuesInjectionResolver())
+        .to(new TypeLiteral<InjectionResolver<ConfigValues>>() {});
 
     this.bindFactory(ResourceConverterFactory.class).to(ResourceConverter.class)
         .in(Singleton.class);
