b05f46d3ce1ae7b41105c87e58d26671bef8d743
Kevin Lotz <lotzk@outlook.com>
2019-04-29 11:24:19 +0200

    Fix missing object Id bug

--- landscape-model/src/main/java/net/explorviz/shared/landscape/model/store/Timestamp.java
+++ landscape-model/src/main/java/net/explorviz/shared/landscape/model/store/Timestamp.java
@@ -1,7 +1,9 @@
 package net.explorviz.shared.landscape.model.store;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.github.jasminb.jsonapi.annotations.Type;
 import net.explorviz.shared.landscape.model.helper.BaseEntity;
 
@@ -11,6 +13,7 @@ import net.explorviz.shared.landscape.model.helper.BaseEntity;
  */
 @SuppressWarnings("serial")
 @Type("timestamp")
+@JsonIdentityInfo(generator = ObjectIdGenerators.StringIdGenerator.class, property = "this.id")
 public class Timestamp extends BaseEntity {
 
   private long timestamp;
