c32ba95ffa514c1ac7ab6b5963953245512f9fab
Alexander Krause <akr@informatik.uni-kiel.de>
2019-04-30 13:46:41 +0200

    Revert "Merge branch 'master' of https://github.com/ExplorViz/explorviz-backend-shared"
    This reverts commit 91462fa1a568f4b9966e1956f4c888870fad9605, reversing
    changes made to a7e8266db518ea9e81ab7c03d18dc770447aaecd.

--- config-injection/src/main/java/net/explorviz/shared/config/annotations/ConfigValues.java
+++ config-injection/src/main/java/net/explorviz/shared/config/annotations/ConfigValues.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.FIELD})
+@Target({ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ConfigValues {
   Config[] value(); // NOCS
