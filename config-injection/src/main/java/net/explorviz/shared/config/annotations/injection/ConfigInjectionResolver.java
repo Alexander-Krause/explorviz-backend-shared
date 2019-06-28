03cc4a00a16ba373306b57e8e68b41cb79c07a6f
Alexander Krause <akr@informatik.uni-kiel.de>
2019-06-28 09:38:48 +0200

    cleanup

--- config-injection/src/main/java/net/explorviz/shared/config/annotations/injection/ConfigInjectionResolver.java
+++ config-injection/src/main/java/net/explorviz/shared/config/annotations/injection/ConfigInjectionResolver.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.inject.Singleton;
