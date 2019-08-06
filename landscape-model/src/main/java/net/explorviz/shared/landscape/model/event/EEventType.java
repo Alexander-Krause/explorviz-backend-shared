e8319df18753b585decc8f83e5fe74dfe0ae9dc5
Kevin Lotz <lotzk@outlook.com>
2019-08-06 10:22:52 +0200

    Move landscape model to sub-module of landscape-service

--- landscape-model/src/main/java/net/explorviz/shared/landscape/model/event/EEventType.java
+++
@@ -1,9 +0,0 @@
-package net.explorviz.shared.landscape.model.event;
-
-/**
- * Enumeration representing supported event types within the data model.
- *
- */
-public enum EEventType {
-	NEWSYSTEM, NEWNODE, NEWAPPLICATION, EXCEPTION, UNKNOWN
-}
